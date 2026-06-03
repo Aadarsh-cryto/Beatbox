62fab34cb2e52e2e799c3ddb4966ace104dc5f60
Aadarsh ojha <aadarshojha46@gmail.com>
2026-06-03 17:20:53 +0530

    backend backend commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   // baseURL: "http://localhost:3000/api",
+//     baseURL: "https://beatbox-project-ls41.onrender.com"
+//   withCredentials: true
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://beatbox-project-ls41.onrender.com",
   withCredentials: true,
 });
 
