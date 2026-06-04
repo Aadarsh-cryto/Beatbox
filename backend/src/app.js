b00ea2914fb3cb23073f2c3da7df8bb6a35dbe20
Aadarsh ojha <aadarshojha46@gmail.com>
2026-06-04 10:15:41 +0530

    Added CORS fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@
  const cors =  require("cors");
  const app = express();
  app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://beatbox-project-1-frontend.onrender.com/",
     credentials:true
  }))
 app.use(express.json());
