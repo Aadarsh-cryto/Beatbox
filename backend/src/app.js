7a16be3143e0b71128e39552a82c810591eda09c
Aadarsh ojha <aadarshojha46@gmail.com>
2026-06-08 10:55:58 +0530

    Production URLs configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@
  const cors =  require("cors");
  const app = express();
  app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://beatbox-murex.vercel.app",
     credentials:true
  }))
 app.use(express.json());
