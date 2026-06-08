4e5ad9e8dee0974afc6d2df32524469f9fecd974
Aadarsh ojha <aadarshojha46@gmail.com>
2026-06-08 09:26:27 +0530

    Initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://beatbox-project-ls41.onrender.com",
+   baseURL: "http://localhost:3000/api",
     
   withCredentials: true
 });
