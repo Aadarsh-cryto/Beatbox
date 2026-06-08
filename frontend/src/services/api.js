71d221743050d682a1ba409a51711f88bf54b5a4
Aadarsh ojha <aadarshojha46@gmail.com>
2026-06-08 10:28:28 +0530

    Update production URLs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://localhost:3000/api",
+   baseURL: "https://beatbox-backend-0hhw.onrender.com",
     
   withCredentials: true
 });
